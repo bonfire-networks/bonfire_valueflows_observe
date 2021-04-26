c57ca18bc8143210d00f4b4563fbde3b4a0ee05a
Mayel <git@mayel.space>
2021-04-26 16:52:28 +0200

    fix

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -26,7 +26,7 @@ defmodule ValueFlows.Observe.ConnCase do
       import ValueFlows.Observe.ConnCase
       import ValueFlows.Observe.Test.ConnHelpers
       # import ValueFlows.Observe.Test.FakeHelpers
-      unquote(Bonfire.Common.Utils.import_if_enabled(Bonfire.Me.Fake))
+      Bonfire.Common.Utils.Bonfire.Common.Utils(Bonfire.Me.Fake)
 
       alias ValueFlows.Observe.Fake
       alias ValueFlows.Observe.Web.Router.Helpers, as: Routes
