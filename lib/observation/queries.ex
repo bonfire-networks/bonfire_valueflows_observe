d9820688a55256c226bdf357287eb6c7b914a78b
Mayel <git@mayel.space>
2021-06-22 23:08:26 +0200

    q

--- lib/observation/queries.ex
+++ lib/observation/queries.ex
@@ -15,7 +15,7 @@ defmodule ValueFlows.Observe.Observation.Queries do
     from(c in Observation, as: :observation)
   end
 
-  def query(filters), do: filter(Observation, filters)
+  def query(filters), do: query(Observation, filters)
 
   def query(q, filters), do: filter(query(q), filters)
 
