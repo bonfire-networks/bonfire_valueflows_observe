1540b181b7e68155445c6b067cefdd5d2494bd0a
Mayel de Borniol <git@mayel.space>
2022-09-29 10:09:54 +1300

    current_user_required

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,6 +1,6 @@
 defmodule ValueFlows.Observe.Integration do
   def is_admin?(user) do
-    if Map.get(user, :instance_admin) do
+    if is_map(user) and Map.get(user, :instance_admin) do
       Map.get(user.instance_admin, :is_instance_admin)
     else
       # FIXME
